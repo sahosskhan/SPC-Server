635cb762f69a77cf8c0c0bd94a9acb4a106188ce
sahosskhan <sahosskhan359@gmail.com>
2024-06-24 22:10:13 +0600

    basic setup, post user data

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+
+//middleware config
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = process.env.DB_URL;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const userCollection = client.db("SpanishCityFCDb").collection("User");
+
+  app.post("/users", async (req, res) => {
+    const user = req.body;
+    const query = { email: user.email };
+    const existingUser = await userCollection.findOne(query);
+    if (existingUser) {
+      return res.send({ message: "User already exists", insertedInd: null });
+    }
+    const result = await userCollection.insertOne(user);
+    res.send(result);
+  });
+                
+
+    // await client.connect();
+
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('🎯 Welcome to  Server🗄️')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Server is working on port ${port}`);
+  })
